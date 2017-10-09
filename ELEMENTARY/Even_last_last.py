7d5b305a21318d4c0348f524621965e8f3a6364c
AlexRITIAN <yuhao5@Lenovo.com>
2017-10-09 18:25:02 +0800

    learn more

--- ELEMENTARY/Even_last_last.py
+++ ELEMENTARY/Even_last_last.py
@@ -9,5 +9,8 @@ def checkio_vol2(array):
         return 0
     return sum(array[i] for i in list(range(0,len(array),2))) * array[len(array) - 1]
 
+def checkio_vol3(array):
+    return sum(array[0::2]) * array[-1] if array else 0
+
 if __name__ == '__main__':
-    print(checkio_vol2([]))
+    print(checkio_vol3([0,1,2,3,4,5]))
--- ELEMENTARY/The_Most_Numbers.py
+++ ELEMENTARY/The_Most_Numbers.py
@@ -2,3 +2,6 @@ def checkio(*args):
     if len(args) == 0:
         return 0
     return sorted(args,reverse=True)[0] - sorted(args,reverse=False)[0]
+
+if __name__ == "__main__":
+    print(checkio(0.0015,0.0025,0.0035))
