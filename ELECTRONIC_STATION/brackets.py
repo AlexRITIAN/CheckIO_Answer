22525e0fb8bc5fbdbf8c299675eee1614be190f9
AlexRITIAN <yuhao5@Lenovo.com>
2017-10-18 18:35:03 +0800

    can't fix yet

--- ELECTRONIC_STATION/brackets.py
+++ ELECTRONIC_STATION/brackets.py
@@ -1,12 +1,30 @@
 def checkio(expression):
+    comparison = {"(":")","[":"]","{":"}"}
     brackets = []
     for char in expression:
-        if char == "{" or char == "[" or char =="(" or char == ")" or char == "]" or char == "}":
+        if char in comparison or char in comparison.values():
             brackets.append(char)
     if len(brackets)%2 != 0:
         return False
-    for i in range(0,int(len(brackets)/2)):
-        if not(brackets[i] == "{" and brackets[len(brackets)-1 - i] == "}") and not(brackets[i] == "[" and brackets[len(brackets)-1 - i] == "]") and not(brackets[i] == "(" and brackets[len(brackets)-1 - i] == ")"):
+    l = int(len(brackets)/2)
+    for n in range(0,l - 1):
+        if brackets[l - 1 -n] in comparison:
+            if brackets[l + n] != comparison[brackets[l - 1 - n]]:
+                print(l)
+                print(n)
+                print(brackets[l + n])
+                print(comparison[brackets[l - 1 - n]])
+                return False
+        elif brackets[l + n] not in comparison:
+            print(l)
+            print(n)
+            print(brackets[l + n])
+            return False
+        elif brackets[l -1 -n] != comparison[brackets[l + n]]:
+            print(l)
+            print(n)
+            print(brackets[l -1 -n])
+            print(comparison[brackets[l + n]])
             return False
     return True
 
@@ -18,5 +36,6 @@ if __name__ == '__main__':
     # assert checkio("[1+1]+(2*2)-{3/3}") == True, "Different operators"
     # assert checkio("(({[(((1)-2)+3)-3]/3}-3)") == False, "One is redundant"
     # assert checkio("2+3") == True, "No brackets, no problem"
-    print(checkio("((5+3)*2+1)"))
+    # print(checkio("((5+3)*2+1)"))
     print(checkio("[1+1]+(2*2)-{3/3}"))
+    # print(checkio("(3+{1-1)}"))
