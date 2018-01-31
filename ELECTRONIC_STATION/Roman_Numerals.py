c433e4843a9b8fa91e3b937dd780f50538e24b3d
AlexRITIAN <yuhao5@Lenovo.com>
2018-01-31 13:32:30 +0800

    donw XD

--- ELECTRONIC_STATION/Roman_Numerals.py
+++ ELECTRONIC_STATION/Roman_Numerals.py
@@ -50,6 +50,15 @@ def checkio_vol2(data):
                 break
     return roman_result
 
+#I dont understand how it work
+def checkio(n):
+    result = ''
+    for arabic, roman in zip((1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1),
+                             'M     CM   D    CD   C    XC  L   XL  X   IX V  IV I'.split()):
+        result += n // arabic * roman
+        n %= arabic
+    return result
+
 if __name__ == '__main__':
     #These "asserts" using only for self-checking and not necessary for auto-testing
     # assert checkio(6) == 'VI', '6'
---
+++ ELECTRONIC_STATION/The_Longest_Palindromic.py
@@ -0,0 +1,59 @@
+'''
+ * @Author: yuhao 
+ * @Date: 2018-01-30 14:00:49 
+ * @Last Modified by:   yuhao 
+ * @Last Modified time: 2018-01-30 14:00:49 
+'''
+
+
+def checkio(data):
+    results = []
+    results_index = []
+    final_list_index = 0
+    max_lent = 0
+    results_string = ""
+    for i in range(len(data)):
+        j = i - 1
+        k = i + 1
+        while j >= 0 and k < len(data):
+            # if data[i] == data[i+1]:
+            left_str = data[j:i+1]
+            right_str = data[i+1:k+2]
+            if left_str == right_str[::-1]:
+                index_list = []
+                results.append(left_str + right_str)
+                index_list.append(j)
+                index_list.append(k+2)
+                results_index.append(index_list)
+            # else:
+            left_str = data[j:i]
+            right_str = data[i+1:k+1]
+            if left_str == right_str[::-1]:
+                index_list = []
+                results.append(left_str + right_str)
+                index_list.append(j)
+                index_list.append(k+1)
+                results_index.append(index_list)
+            j = j - 1
+            k = k + 1
+    i = 0
+    if 0 == len(results_index):
+        return data[0]
+    else:
+        for in_list in results_index:
+            lent = in_list[1] - in_list[0]
+            if max_lent < lent:
+                max_lent = lent
+                final_list_index = i
+            i = i + 1
+        results_string = data[results_index[final_list_index][0]:results_index[final_list_index][1]]
+        return results_string
+
+if __name__ == "__main__":
+    print(checkio("abarada"))
+    print(checkio("artrartrt"))
+    print(checkio("aaaa"))
+    print(checkio("abc"))
+    print(checkio("aaaaa"))
+
+    
